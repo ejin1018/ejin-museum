c770ccce4e44962c3b7024f683ed57141c0a8618
ejin1018 <amy1018@naver.com>
2023-01-17 23:47:09 +0900

    gnb 하단 여백 채우기

--- js/subPage.js
+++ js/subPage.js
@@ -2,10 +2,10 @@ $(function(){
   $(window).scroll(function(){
     var scrollTop = $(window).scrollTop();
     var utilHeight = $('.utility-wrap').height();
-    // if(scrollTop>=utilHeight){
-    //   $('.header-wrap').css('position','fixed');
-    // }else{
-    //   $('.header-wrap').css('position','relative');
-    // }
+    if(scrollTop>=utilHeight){
+      $('.header-wrap').css('position','fixed');
+    }else{
+      $('.header-wrap').css('position','relative');
+    }
   })
 })
