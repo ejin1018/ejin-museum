7c64587ad5f146641cacce1dfc011fd5240bc888
ejin1018 <amy1018@naver.com>
2023-01-21 21:20:04 +0900

    서브 페이지 픽스 헤더 구현

--- js/subPage.js
+++ js/subPage.js
@@ -3,9 +3,11 @@ $(function(){
     var scrollTop = $(window).scrollTop();
     var utilHeight = $('.utility-wrap').height();
     if(scrollTop>=utilHeight){
-      $('.header-wrap').css('position','fixed');
+      $('.header-wrap').addClass('head-fixed');
+      $('.subPage-contents-wrap').addClass('cont-fixed');
     }else{
-      $('.header-wrap').css('position','relative');
+      $('.header-wrap').removeClass('head-fixed');
+      $('.subPage-contents-wrap').removeClass('cont-fixed');
     }
   })
 })
