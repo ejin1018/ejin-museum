7dcf7e2c3ae04bba5b11070a3c3f9319f4f21ea9
ejin1018 <amy1018@naver.com>
2023-01-25 23:11:57 +0900

    메뉴 스크롤 반응형 맞춤, 서브 페이지 닫기 버튼 추가

--- js/subPage.js
+++ js/subPage.js
@@ -1,13 +1,16 @@
 $(function(){
   $(window).scroll(function(){
-    var scrollTop = $(window).scrollTop();
-    var utilHeight = $('.utility-wrap').height();
-    if(scrollTop>=utilHeight){
-      $('.header-wrap').addClass('head-fixed');
-      $('.subPage-contents-wrap').addClass('cont-fixed');
-    }else{
-      $('.header-wrap').removeClass('head-fixed');
-      $('.subPage-contents-wrap').removeClass('cont-fixed');
+    if(window.innerWidth >=980){
+      var scrollTop = $(window).scrollTop();
+      var utilHeight = $('.utility-wrap').height();
+      if(scrollTop>=utilHeight){
+        $('.header-wrap').addClass('head-fixed');
+        $('.subPage-contents-wrap').addClass('cont-fixed');
+      }else{
+        $('.header-wrap').removeClass('head-fixed');
+        $('.subPage-contents-wrap').removeClass('cont-fixed');
+      }
+
     }
   })
 })
