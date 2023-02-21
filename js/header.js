4c7e92a9510da769d9a3c7fbd225ff18f2a258a3
ejin <amy1018@naver.com>
2023-02-21 12:02:09 +0900

    헤더 메인에서도 fixed

--- js/header.js
+++ js/header.js
@@ -8,6 +8,21 @@ $(function(){
     },500);
   });
 
+  // header sticky
+  $(window).scroll(function(){
+    if(window.innerWidth >=980){
+      var scrollTop = $(window).scrollTop();
+      var utilHeight = $('.utility-wrap').height();
+      if(scrollTop>=utilHeight){
+        $('.header-wrap').addClass('head-fixed');
+        $('.subPage-contents-wrap').addClass('cont-fixed');
+      }else{
+        $('.header-wrap').removeClass('head-fixed');
+        $('.subPage-contents-wrap').removeClass('cont-fixed');
+      }
+    }
+  })
+
   //search
   var searchBtn = $('.search-btn');
   var searchLayer = $('.search-layer');
